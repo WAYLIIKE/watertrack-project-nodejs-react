a06cfd6a6763154669bc8001ae76e51946865544
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2024-05-09 01:21:53 +0200

    initialized vite react project config

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react_vite">
+    <BrowserRouter basename="/watertrack-project-nodejs-react">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
