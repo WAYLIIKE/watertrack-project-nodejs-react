58fbf3abb3029bbc5d22e0ea2bb36bbf168593dd
IrinaLender27 <irina.lender27@gmail.com>
2024-05-09 23:41:05 +0300

    update modal

--- src/components/BaseModal/BaseModal.jsx
+++ src/components/BaseModal/BaseModal.jsx
@@ -1,47 +1,25 @@
-import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import css from './BaseModal.module.css';
-import { ModalBackdrop } from '../ModalBackdrop/ModalBackdrop';
 import { IconClose } from '../Icons/IconClose';
+import Modal from 'react-modal';
 
 export const BaseModal = ({ isOpen, onClose, children }) => {
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.key === 'Escape') {
-        onClose();
-      }
-    };
-    if (isOpen) {
-      document.addEventListener('keydown', handleKeyDown);
-    }
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [isOpen, onClose]);
-
-  const handleClose = () => {
-    onClose();
-  };
-  const handleBackdropClick = (event) => {
-    if (event.target === event.currentTarget) {
-      onClose();
-    }
-  };
-
-  return isOpen ? (
-    <ModalBackdrop onClick={handleBackdropClick}>
-      <div className={css.modal}>
-        <div className={css.modalContent}>
-          {children}
-          <IconClose className={css.button} onClick={handleClose}>
-            Close
-          </IconClose>
-        </div>
-      </div>
-    </ModalBackdrop>
-  ) : null;
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={() => onClose()}
+      className={css.modalContent}
+      closeTimeoutMS={300}
+      ariaHideApp={false}
+      overlayClassName={css.overlay}
+    >
+      <button onClick={() => onClose()}>
+        <IconClose className={css.button} />
+      </button>
+      {children}
+    </Modal>
+  );
 };
-
 BaseModal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
