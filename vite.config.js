89bbc9fc7af705be4d4e796345beaf607dbf145b
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2024-05-09 03:50:06 +0200

    Update project cfg

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import reactIcons from 'vite-plugin-react-icons';
 
 export default defineConfig({
-  plugins: [react(), svgr(), reactRefresh(), reactIcons()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       src: '/src',
