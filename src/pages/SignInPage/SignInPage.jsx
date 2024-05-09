dd18cfeff9a34d967fe91aea2ec5a86a63c5b2d2
Maksym <140002546+Machokar@users.noreply.github.com>
2024-05-09 15:28:27 +0100

    up

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,12 +1,76 @@
-import { Helmet } from 'react-helmet-async';
-
+import { Field, Form, Formik } from 'formik';
+import { useId } from 'react';
+import css from './SignInPage.module.css';
+import * as Yup from 'yup';
+import { NavLink } from 'react-router-dom';
+const loginSchema = Yup.object().shape({
+  email: Yup.string()
+    .min(3, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  password: Yup.string()
+    .min(3, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+});
 export default function SignInPage() {
+  const emailId = useId();
+  const passId = useId();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('good job');
+  };
   return (
-    <div>
-      <p>Sign in page...</p>
-      <Helmet>
-        <title>Sign in</title>
-      </Helmet>
-    </div>
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+      }}
+      validationSchema={loginSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form
+        className={css.signinForm}
+        onSubmit={handleSubmit}
+        autoComplete="on"
+      >
+        <h1 className={css.signinName}>Sign In</h1>
+        <div className={css.signinFormGroup}>
+          <label className={css.signinLabel} htmlFor={emailId}>
+            Email:
+          </label>
+          <Field
+            type="text"
+            name="email"
+            className={css.signinField}
+            id={emailId}
+            autoComplete="username"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className={css.signinFormGroup}>
+          <label className={css.signinLabel} htmlFor={passId}>
+            Password:
+          </label>
+          <Field
+            type="password"
+            name="password"
+            className={css.signinField}
+            id={passId}
+            autoComplete="current-password"
+            placeholder="Enter your password"
+          />
+        </div>
+        <button className={css.signinButton} type="submit">
+          Log In
+        </button>
+        <p className={css.signinText}>
+          Don’t have an account?
+          <NavLink to="/signup" className={css.signinA}>
+            Sign Up
+          </NavLink>
+        </p>
+      </Form>
+    </Formik>
   );
 }
--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,12 +1,66 @@
-import { Helmet } from 'react-helmet-async';
-
+import { Field, Form, Formik } from 'formik';
+import { useId } from 'react';
+import css from './SignUpPage.module.css';
+import * as Yup from 'yup';
+const registerSchema = Yup.object().shape({
+  email: Yup.string()
+    .min(3, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  password: Yup.string()
+    .min(3, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+});
 export default function SignUpPage() {
+  const emailId = useId();
+  const passwordId = useId();
+  const passwordRepeatId = useId();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <p>Sign up page...</p>
-      <Helmet>
-        <title>Sign up</title>
-      </Helmet>
-    </div>
+    <Formik
+      initialValues={{
+        name: '',
+        email: '',
+        password: '',
+      }}
+      validationSchema={registerSchema}
+    >
+      <Form className={css.signupForm} onSubmit={handleSubmit}>
+        <div className={css.signupFormGroup}>
+          <label className={css.signupLabel} htmlFor={emailId}>
+            Email
+          </label>
+          <Field
+            type="text"
+            name="email"
+            id={emailId}
+            autoComplete="username"
+          />
+        </div>
+        <div className={css.signupFormGroup}>
+          <label className={css.signupLabel} htmlFor={passwordId}>
+            Password
+          </label>
+          <Field
+            type="password"
+            name="password"
+            id={passwordId}
+            autoComplete="current-password"
+          />
+        </div>
+        <div className={css.signupFormGroup}>
+          <label className={css.signupLabel} htmlFor={passwordRepeatId}>
+            Repeat password
+          </label>
+          <Field type="password" name="password" id={passwordRepeatId} />
+        </div>
+        <button className={css.signupButton} type="submit">
+          Register
+        </button>
+      </Form>
+    </Formik>
   );
 }
