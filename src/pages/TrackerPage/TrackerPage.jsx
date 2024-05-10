9e13e62c158c796dc552f6af117363902e7e0e18
anya-sem <139631738+anya-sem@users.noreply.github.com>
2024-05-10 23:25:12 +0300

    add delete&logout modal

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,11 +1,15 @@
 import { Helmet } from 'react-helmet-async';
 import { WaterModal } from '../../components/WaterModal/WaterModal';
+import { DeleteWaterModal } from '../../components/DeleteWaterModal/DeleteWaterModal';
+import { LogOutModal } from '../../components/LogOutModal/LogOutModal';
 import { Container } from '../../components/Container/Container';
 import { BaseModal } from '../../components/BaseModal/BaseModal';
 import { useState } from 'react';
 
 export default function TrackerPage() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
 
   const openModal = () => {
     setIsOpen(true);
@@ -13,6 +17,22 @@ export default function TrackerPage() {
 
   const closeModal = () => {
     setIsOpen(false);
+  };
+
+  const openDeleteModal = () => {
+    setIsDeleteModalOpen(true);
+  };
+
+  const closeDeleteModal = () => {
+    setIsDeleteModalOpen(false);
+  };
+
+  const openLogoutModal = () => {
+    setIsLogoutModalOpen(true);
+  };
+
+  const closeLogoutModal = () => {
+    setIsLogoutModalOpen(false);
   };
 
   return (
@@ -24,8 +44,20 @@ export default function TrackerPage() {
       <button type="button" onClick={openModal}>
         Open
       </button>
+      <button type="button" onClick={openDeleteModal}>
+        Delete Water
+      </button>
+      <button type="button" onClick={openLogoutModal}>
+        Log out
+      </button>
       <BaseModal isOpen={isOpen} onClose={closeModal}>
         <WaterModal title={'Add water'} />
+      </BaseModal>
+      <BaseModal isOpen={isDeleteModalOpen} onClose={closeDeleteModal}>
+        <DeleteWaterModal title={'Delete entry'} />
+      </BaseModal>
+      <BaseModal isOpen={isLogoutModalOpen} onClose={closeLogoutModal}>
+        <LogOutModal title={'Log out'} />
       </BaseModal>
     </Container>
   );
