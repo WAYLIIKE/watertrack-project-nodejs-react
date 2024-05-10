35f8e62b54360f8b678154b9e2ce9de933ab72ad
IrinaLender27 <irina.lender27@gmail.com>
2024-05-10 23:07:07 +0300

    update

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -3,36 +3,38 @@ import { girl1, girl2, boy } from '../../assets/img';
 export const AdvantagesSection = () => {
   return (
     <div className={css.section}>
-      <div>
-        <div className={css.studentsBox}>
-          <ul className={css.studentImg}>
-            <li className={css.studenItem}>
-              <img className={css.img} src={girl2} alt="girl1" />
-            </li>
-            <li className={css.studenItem}>
-              <img className={css.img} src={boy} alt="boy" />
-            </li>
-            <li className={css.studenItem}>
-              <img className={css.img} src={girl1} alt="girl2" />
-            </li>
-          </ul>
-          <p>Our happy customers</p>
-        </div>
-        <div>
-          <ul>
-            <li className={css.textHabits}>
-              <div className={css.ellipse}></div>
-              <p className={css.text}>Habit drive</p>
-            </li>
-            <li className={css.textStatistic}>
-              <p className={css.textBlack}>View statistics</p>
-            </li>
-            <li className={css.textSetting}>
-              <p className={css.textBlack}>Personal rate setting</p>
-            </li>
-          </ul>
-        </div>
+      <div className={css.studentsBox}>
+        <ul className={css.customerImg}>
+          <li className={css.item}>
+            <img className={css.img} src={girl2} alt="girl1" />
+          </li>
+
+          <li className={css.item}>
+            <img className={css.img} src={boy} alt="boy" />
+          </li>
+
+          <li className={css.item}>
+            <img className={css.img} src={girl1} alt="girl2" />
+          </li>
+        </ul>
+
+        <p className={css.customerText}>
+          Our <span>happy</span> customers
+        </p>
       </div>
+
+      <ul className={css.advantagesGroup}>
+        <li className={css.textAdvantage}>
+          <div className={css.ellipse}></div>
+          <p className={css.text}>Habit drive</p>
+        </li>
+        <li className={css.textAdvantage}>
+          <p className={css.textBlack}>View statistics</p>
+        </li>
+        <li className={css.textAdvantage}>
+          <p className={css.textBlack}>Personal rate setting</p>
+        </li>
+      </ul>
     </div>
   );
 };
