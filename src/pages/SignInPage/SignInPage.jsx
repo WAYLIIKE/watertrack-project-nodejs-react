166d4164a8af9297204721d1d31c20e16f405404
Maksym <140002546+Machokar@users.noreply.github.com>
2024-05-10 13:16:29 +0100

    up

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,76 +1,106 @@
-import { Field, Form, Formik } from 'formik';
+import { ErrorMessage, Field, Form, Formik, getIn } from 'formik';
 import { useId } from 'react';
 import css from './SignInPage.module.css';
 import * as Yup from 'yup';
 import { NavLink } from 'react-router-dom';
-const loginSchema = Yup.object().shape({
-  email: Yup.string()
-    .min(3, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  password: Yup.string()
-    .min(3, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-});
+
 export default function SignInPage() {
   const emailId = useId();
   const passId = useId();
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('good job');
-  };
+  const loginSchema = Yup.object().shape({
+    email: Yup.string()
+      .email()
+      .matches('^(?!.*@[^,]*,)', 'Invalid email')
+      .required('Email is required'),
+    password: Yup.string()
+      .required('No password provided.')
+      .min(8, 'Password is too short - should be 8 chars minimum.')
+      .matches('[a-zA-Z]', 'Password can only contain Latin letters.'),
+  });
   return (
-    <Formik
-      initialValues={{
-        email: '',
-        password: '',
-      }}
-      validationSchema={loginSchema}
-      onSubmit={handleSubmit}
-    >
-      <Form
-        className={css.signinForm}
-        onSubmit={handleSubmit}
-        autoComplete="on"
+    <div className={css.signinBack}>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validationSchema={loginSchema}
+        onSubmit={(values, action) => {
+          console.log('login success');
+          action.resetForm();
+        }}
       >
-        <h1 className={css.signinName}>Sign In</h1>
-        <div className={css.signinFormGroup}>
-          <label className={css.signinLabel} htmlFor={emailId}>
-            Email:
-          </label>
-          <Field
-            type="text"
-            name="email"
-            className={css.signinField}
-            id={emailId}
-            autoComplete="username"
-            placeholder="Enter your email"
-          />
-        </div>
-        <div className={css.signinFormGroup}>
-          <label className={css.signinLabel} htmlFor={passId}>
-            Password:
-          </label>
-          <Field
-            type="password"
-            name="password"
-            className={css.signinField}
-            id={passId}
-            autoComplete="current-password"
-            placeholder="Enter your password"
-          />
-        </div>
-        <button className={css.signinButton} type="submit">
-          Log In
-        </button>
-        <p className={css.signinText}>
-          Don’t have an account?
-          <NavLink to="/signup" className={css.signinA}>
-            Sign Up
-          </NavLink>
-        </p>
-      </Form>
-    </Formik>
+        <Form className={css.signinForm} autoComplete="on">
+          <h1 className={css.signinName}>Sign In</h1>
+          <div className={css.signinFormGroup}>
+            <label className={css.signinLabel} htmlFor={emailId}>
+              Email:
+            </label>
+            <Field name="email">
+              {({ field, form }) => {
+                const error = getIn(form.errors, field.name);
+                const touched = getIn(form.touched, field.name);
+                const isError = error && touched;
+                return (
+                  <input
+                    {...field}
+                    type="text"
+                    className={`${css.signinField} ${
+                      isError ? css.signInErrorField : ''
+                    }`}
+                    id={emailId}
+                    autoComplete="username"
+                    placeholder="Enter your email"
+                  />
+                );
+              }}
+            </Field>
+            <ErrorMessage
+              name="email"
+              component="span"
+              className={css.signinError}
+            />
+          </div>
+          <div className={css.signinFormGroup}>
+            <label className={css.signinLabel} htmlFor={passId}>
+              Password:
+            </label>
+            <Field name="password">
+              {({ field, form }) => {
+                const error = getIn(form.errors, field.name);
+                const touched = getIn(form.touched, field.name);
+                const isError = error && touched;
+                return (
+                  <input
+                    {...field}
+                    type="password"
+                    className={`${css.signinField} ${
+                      isError ? css.signInErrorField : ''
+                    }`}
+                    id={passId}
+                    autoComplete="current-password"
+                    placeholder="Enter your password"
+                  />
+                );
+              }}
+            </Field>
+            <ErrorMessage
+              name="password"
+              component="span"
+              className={css.signinError}
+            />
+          </div>
+          <button className={css.signinButton} type="submit">
+            Log In
+          </button>
+          <p className={css.signinText}>
+            Don’t have an account?
+            <NavLink to="/signup" className={css.signinA}>
+              Sign Up
+            </NavLink>
+          </p>
+        </Form>
+      </Formik>
+    </div>
   );
 }
--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,66 +1,138 @@
-import { Field, Form, Formik } from 'formik';
+import { ErrorMessage, Field, Form, Formik, getIn } from 'formik';
 import { useId } from 'react';
 import css from './SignUpPage.module.css';
 import * as Yup from 'yup';
-const registerSchema = Yup.object().shape({
-  email: Yup.string()
-    .min(3, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  password: Yup.string()
-    .min(3, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-});
+import { NavLink } from 'react-router-dom';
+
 export default function SignUpPage() {
   const emailId = useId();
   const passwordId = useId();
   const passwordRepeatId = useId();
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  const registerSchema = Yup.object().shape({
+    email: Yup.string()
+      .email()
+      .matches('^(?!.*@[^,]*,)', 'Invalid email')
+      .required('Email is required'),
+    password: Yup.string()
+      .required('No password provided.')
+      .min(8, 'Password is too short - should be 8 chars minimum.')
+      .matches('[a-zA-Z]', 'Password can only contain Latin letters.'),
+    passwordRepeat: Yup.string().required('No password provided'),
+  });
   return (
-    <Formik
-      initialValues={{
-        name: '',
-        email: '',
-        password: '',
-      }}
-      validationSchema={registerSchema}
-    >
-      <Form className={css.signupForm} onSubmit={handleSubmit}>
-        <div className={css.signupFormGroup}>
-          <label className={css.signupLabel} htmlFor={emailId}>
-            Email
-          </label>
-          <Field
-            type="text"
-            name="email"
-            id={emailId}
-            autoComplete="username"
-          />
-        </div>
-        <div className={css.signupFormGroup}>
-          <label className={css.signupLabel} htmlFor={passwordId}>
-            Password
-          </label>
-          <Field
-            type="password"
-            name="password"
-            id={passwordId}
-            autoComplete="current-password"
-          />
-        </div>
-        <div className={css.signupFormGroup}>
-          <label className={css.signupLabel} htmlFor={passwordRepeatId}>
-            Repeat password
-          </label>
-          <Field type="password" name="password" id={passwordRepeatId} />
-        </div>
-        <button className={css.signupButton} type="submit">
-          Register
-        </button>
-      </Form>
-    </Formik>
+    <div className={css.signupBack}>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+          passwordRepeat: '',
+        }}
+        validationSchema={registerSchema}
+        onSubmit={(values, action) => {
+          console.log('register success');
+          action.resetForm();
+        }}
+      >
+        <Form className={css.signupForm}>
+          <h1 className={css.signupName}>Sign up</h1>
+          <div className={css.signupFormGroup}>
+            <label className={css.signupLabel} htmlFor={emailId}>
+              Email
+            </label>
+            <Field name="email">
+              {({ field, form }) => {
+                const error = getIn(form.errors, field.name);
+                const touched = getIn(form.touched, field.name);
+                const isError = error && touched;
+                return (
+                  <input
+                    {...field}
+                    type="text"
+                    className={`${css.signupField} ${
+                      isError ? css.signUpErrorField : ''
+                    }`}
+                    id={emailId}
+                    autoComplete="username"
+                    placeholder="Enter your email"
+                  />
+                );
+              }}
+            </Field>
+            <ErrorMessage
+              name="email"
+              component="span"
+              className={css.signupError}
+            />
+          </div>
+          <div className={css.signupFormGroup}>
+            <label className={css.signupLabel} htmlFor={passwordId}>
+              Password
+            </label>
+            <Field name="password">
+              {({ field, form }) => {
+                const error = getIn(form.errors, field.name);
+                const touched = getIn(form.touched, field.name);
+                const isError = error && touched;
+                return (
+                  <input
+                    {...field}
+                    type="password"
+                    className={`${css.signupField} ${
+                      isError ? css.signUpErrorField : ''
+                    }`}
+                    id={emailId}
+                    autoComplete="current-password"
+                    placeholder="Enter your password"
+                  />
+                );
+              }}
+            </Field>
+            <ErrorMessage
+              name="password"
+              component="span"
+              className={css.signupError}
+            />
+          </div>
+          <div className={css.signupFormGroup}>
+            <label className={css.signupLabel} htmlFor={passwordRepeatId}>
+              Repeat password
+            </label>
+            <Field name="passwordRepeat">
+              {({ field, form }) => {
+                const error = getIn(form.errors, field.name);
+                const touched = getIn(form.touched, field.name);
+                const isError = error && touched;
+                return (
+                  <input
+                    {...field}
+                    type="password"
+                    className={`${css.signupField} ${
+                      isError ? css.signUpErrorField : ''
+                    }`}
+                    id={emailId}
+                    autoComplete="current-password"
+                    placeholder="Repeat password"
+                  />
+                );
+              }}
+            </Field>
+            <ErrorMessage
+              name="password"
+              component="span"
+              className={css.signupError}
+            />
+          </div>
+          <button className={css.signupButton} type="submit">
+            Sign Up
+          </button>
+          <p className={css.signupText}>
+            Don’t have an account?
+            <NavLink to="/signin" className={css.signupA}>
+              Sign Up
+            </NavLink>
+          </p>
+        </Form>
+      </Formik>
+    </div>
   );
 }
