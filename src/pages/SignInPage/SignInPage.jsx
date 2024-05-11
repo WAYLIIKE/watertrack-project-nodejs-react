a8947569a5906d1118d3948d7102be107644b6b5
Maksym <140002546+Machokar@users.noreply.github.com>
2024-05-11 12:15:08 +0100

    up

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -23,55 +23,26 @@ export default function SignInPage() {
     setShowPassword(!showPassword);
   };
   return (
-    <div className={css.signinBack}>
-      <Formik
-        initialValues={{
-          email: '',
-          password: '',
-        }}
-        validationSchema={loginSchema}
-        onSubmit={(values, action) => {
-          console.log('login success');
-          action.resetForm();
-        }}
-      >
-        <Form className={css.signinForm} autoComplete="on">
-          <h1 className={css.signinName}>Sign In</h1>
-          <div className={css.signinFormGroupEmail}>
-            <label className={css.signinLabel} htmlFor={emailId}>
-              Email
-            </label>
-            <Field name="email">
-              {({ field, form }) => {
-                const error = getIn(form.errors, field.name);
-                const touched = getIn(form.touched, field.name);
-                const isError = error && touched;
-                return (
-                  <input
-                    {...field}
-                    type="text"
-                    className={`${css.signinField} ${
-                      isError ? css.signInErrorField : ''
-                    }`}
-                    id={emailId}
-                    autoComplete="username"
-                    placeholder="Enter your email"
-                  />
-                );
-              }}
-            </Field>
-            <ErrorMessage
-              name="email"
-              component="span"
-              className={css.signinError}
-            />
-          </div>
-          <div className={css.signinFormGroupPassword}>
-            <label className={css.signinLabel} htmlFor={passId}>
-              Password
-            </label>
-            <div className={css.signinIconEyes}>
-              <Field name="password">
+    <div className={css.signinLaptop}>
+      <div className={css.signinBack}>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          validationSchema={loginSchema}
+          onSubmit={(values, action) => {
+            console.log('login success');
+            action.resetForm();
+          }}
+        >
+          <Form className={css.signinForm} autoComplete="on">
+            <h1 className={css.signinName}>Sign In</h1>
+            <div className={css.signinFormGroupEmail}>
+              <label className={css.signinLabel} htmlFor={emailId}>
+                Email
+              </label>
+              <Field name="email">
                 {({ field, form }) => {
                   const error = getIn(form.errors, field.name);
                   const touched = getIn(form.touched, field.name);
@@ -79,42 +50,75 @@ export default function SignInPage() {
                   return (
                     <input
                       {...field}
-                      type={showPassword ? 'text' : 'password'}
+                      type="text"
                       className={`${css.signinField} ${
                         isError ? css.signInErrorField : ''
                       }`}
-                      id={passId}
-                      autoComplete="current-password"
-                      placeholder="Enter your password"
+                      id={emailId}
+                      autoComplete="username"
+                      placeholder="Enter your email"
                     />
                   );
                 }}
               </Field>
-              <div
-                className={css.signinIcon}
-                onClick={togglePasswordVisibility}
-              >
-                {showPassword ? <IconEyeClose /> : <IconEye />}
-              </div>
+              <ErrorMessage
+                name="email"
+                component="span"
+                className={css.signinError}
+              />
             </div>
-            <ErrorMessage
-              name="password"
-              component="span"
-              className={css.signinError}
-            />
-          </div>
-          <button className={css.signinButton} type="submit">
-            Log In
-          </button>
-          <p className={css.signinText}>
-            Don’t have an account?
-            <NavLink to="/signup" className={css.signinA}>
-              Sign Up
-            </NavLink>
-          </p>
-        </Form>
-      </Formik>
-      <div></div>
+            <div className={css.signinFormGroupPassword}>
+              <label className={css.signinLabel} htmlFor={passId}>
+                Password
+              </label>
+              <div className={css.signinIconEyes}>
+                <Field name="password">
+                  {({ field, form }) => {
+                    const error = getIn(form.errors, field.name);
+                    const touched = getIn(form.touched, field.name);
+                    const isError = error && touched;
+                    return (
+                      <input
+                        {...field}
+                        type={showPassword ? 'text' : 'password'}
+                        className={`${css.signinField} ${
+                          isError ? css.signInErrorField : ''
+                        }`}
+                        id={passId}
+                        autoComplete="current-password"
+                        placeholder="Enter your password"
+                      />
+                    );
+                  }}
+                </Field>
+                <div
+                  className={css.signinIcon}
+                  onClick={togglePasswordVisibility}
+                >
+                  {showPassword ? <IconEyeClose /> : <IconEye />}
+                </div>
+              </div>
+              <ErrorMessage
+                name="password"
+                component="span"
+                className={css.signinError}
+              />
+            </div>
+            <button className={css.signinButton} type="submit">
+              Log In
+            </button>
+            <p className={css.signinText}>
+              Don’t have an account?
+              <NavLink to="/signup" className={css.signinA}>
+                Sign Up
+              </NavLink>
+            </p>
+          </Form>
+        </Formik>
+      </div>
+      <div className={css.signinPicture}>
+        <h1>afjawnfiawfiawf aiwfbaiwbfiawf awhufbwau</h1>
+      </div>
     </div>
   );
 }
--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -32,60 +32,31 @@ export default function SignUpPage() {
     setshowPasswordReap(!showPasswordReap);
   };
   return (
-    <div className={css.signupBack}>
-      <Formik
-        initialValues={{
-          email: '',
-          password: '',
-          passwordRepeat: '',
-        }}
-        validationSchema={registerSchema}
-        onSubmit={(values, action) => {
-          if (values.password !== values.passwordRepeat) {
-            action.setFieldError('passwordRepeat', 'Passwords do not match');
-          } else {
-            console.log('register success');
-            action.resetForm();
-          }
-        }}
-      >
-        <Form className={css.signupForm}>
-          <h1 className={css.signupName}>Sign up</h1>
-          <div className={css.signupFormGroupEmail}>
-            <label className={css.signupLabel} htmlFor={emailId}>
-              Email
-            </label>
-            <Field name="email">
-              {({ field, form }) => {
-                const error = getIn(form.errors, field.name);
-                const touched = getIn(form.touched, field.name);
-                const isError = error && touched;
-                return (
-                  <input
-                    {...field}
-                    type="text"
-                    className={`${css.signupField} ${
-                      isError ? css.signUpErrorField : ''
-                    }`}
-                    id={emailId}
-                    autoComplete="username"
-                    placeholder="Enter your email"
-                  />
-                );
-              }}
-            </Field>
-            <ErrorMessage
-              name="email"
-              component="span"
-              className={css.signupError}
-            />
-          </div>
-          <div className={css.signupFormGroupEmail}>
-            <label className={css.signupLabel} htmlFor={passwordId}>
-              Password
-            </label>
-            <div className={css.signupIconEyes}>
-              <Field name="password">
+    <div className={css.signupLaptop}>
+      <div className={css.signupBack}>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+            passwordRepeat: '',
+          }}
+          validationSchema={registerSchema}
+          onSubmit={(values, action) => {
+            if (values.password !== values.passwordRepeat) {
+              action.setFieldError('passwordRepeat', 'Passwords do not match');
+            } else {
+              console.log('register success');
+              action.resetForm();
+            }
+          }}
+        >
+          <Form className={css.signupForm}>
+            <h1 className={css.signupName}>Sign up</h1>
+            <div className={css.signupFormGroupEmail}>
+              <label className={css.signupLabel} htmlFor={emailId}>
+                Email
+              </label>
+              <Field name="email">
                 {({ field, form }) => {
                   const error = getIn(form.errors, field.name);
                   const touched = getIn(form.touched, field.name);
@@ -93,78 +64,112 @@ export default function SignUpPage() {
                   return (
                     <input
                       {...field}
-                      type={showPassword ? 'text' : 'password'}
+                      type="text"
                       className={`${css.signupField} ${
                         isError ? css.signUpErrorField : ''
                       }`}
-                      id={passwordId}
-                      autoComplete="new-password"
-                      placeholder="Enter your password"
+                      id={emailId}
+                      autoComplete="username"
+                      placeholder="Enter your email"
                     />
                   );
                 }}
               </Field>
-              <div
-                className={css.signupIcon}
-                onClick={togglePasswordVisibility}
-              >
-                {showPassword ? <IconEyeClose /> : <IconEye />}
-              </div>
+              <ErrorMessage
+                name="email"
+                component="span"
+                className={css.signupError}
+              />
             </div>
-            <ErrorMessage
-              name="password"
-              component="span"
-              className={css.signupError}
-            />
-          </div>
-          <div className={css.signupFormGroup}>
-            <label className={css.signupLabel} htmlFor={passwordRepeatId}>
-              Repeat password
-            </label>
-            <div className={css.signupIconEyes}>
-              <Field name="passwordRepeat">
-                {({ field, form }) => {
-                  const error = getIn(form.errors, field.name);
-                  const touched = getIn(form.touched, field.name);
-                  const isError = error && touched;
-                  return (
-                    <input
-                      {...field}
-                      type={showPasswordReap ? 'text' : 'password'}
-                      className={`${css.signupField} ${
-                        isError ? css.signUpErrorField : ''
-                      }`}
-                      id={passwordRepeatId}
-                      autoComplete="new-password"
-                      placeholder="Repeat password"
-                    />
-                  );
-                }}
-              </Field>
-              <div
-                className={css.signupIcon}
-                onClick={togglePasswordVisibilityP}
-              >
-                {showPasswordReap ? <IconEyeClose /> : <IconEye />}
+            <div className={css.signupFormGroupEmail}>
+              <label className={css.signupLabel} htmlFor={passwordId}>
+                Password
+              </label>
+              <div className={css.signupIconEyes}>
+                <Field name="password">
+                  {({ field, form }) => {
+                    const error = getIn(form.errors, field.name);
+                    const touched = getIn(form.touched, field.name);
+                    const isError = error && touched;
+                    return (
+                      <input
+                        {...field}
+                        type={showPassword ? 'text' : 'password'}
+                        className={`${css.signupField} ${
+                          isError ? css.signUpErrorField : ''
+                        }`}
+                        id={passwordId}
+                        autoComplete="new-password"
+                        placeholder="Enter your password"
+                      />
+                    );
+                  }}
+                </Field>
+                <div
+                  className={css.signupIcon}
+                  onClick={togglePasswordVisibility}
+                >
+                  {showPassword ? <IconEyeClose /> : <IconEye />}
+                </div>
               </div>
+              <ErrorMessage
+                name="password"
+                component="span"
+                className={css.signupError}
+              />
             </div>
-            <ErrorMessage
-              name="passwordRepeat"
-              component="span"
-              className={css.signupError}
-            />
-          </div>
-          <button className={css.signupButton} type="submit">
-            Sign Up
-          </button>
-          <p className={css.signupText}>
-            Already have an account?{' '}
-            <NavLink to="/signin" className={css.signupA}>
-              Sign In
-            </NavLink>
-          </p>
-        </Form>
-      </Formik>
+            <div className={css.signupFormGroup}>
+              <label className={css.signupLabel} htmlFor={passwordRepeatId}>
+                Repeat password
+              </label>
+              <div className={css.signupIconEyes}>
+                <Field name="passwordRepeat">
+                  {({ field, form }) => {
+                    const error = getIn(form.errors, field.name);
+                    const touched = getIn(form.touched, field.name);
+                    const isError = error && touched;
+                    return (
+                      <input
+                        {...field}
+                        type={showPasswordReap ? 'text' : 'password'}
+                        className={`${css.signupField} ${
+                          isError ? css.signUpErrorField : ''
+                        }`}
+                        id={passwordRepeatId}
+                        autoComplete="new-password"
+                        placeholder="Repeat password"
+                      />
+                    );
+                  }}
+                </Field>
+                <div
+                  className={css.signupIcon}
+                  onClick={togglePasswordVisibilityP}
+                >
+                  {showPasswordReap ? <IconEyeClose /> : <IconEye />}
+                </div>
+              </div>
+              <ErrorMessage
+                name="passwordRepeat"
+                component="span"
+                className={css.signupError}
+              />
+            </div>
+            <button className={css.signupButton} type="submit">
+              Sign Up
+            </button>
+            <p className={css.signupText}>
+              Already have an account?{' '}
+              <NavLink to="/signin" className={css.signupA}>
+                Sign In
+              </NavLink>
+            </p>
+          </Form>
+        </Formik>
+      </div>
+      <div className={css.signupPicture}>
+        <h1>afia9ouifhiauwfba8wfbiawf awya8wfgauwi</h1>
+      </div>
     </div>
   );
 }
