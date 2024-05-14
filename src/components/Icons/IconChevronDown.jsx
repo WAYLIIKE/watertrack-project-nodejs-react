5afebc8ae9e343346b3a4bc37f82ab283e27622e
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-05-15 02:15:17 +0300

    IconChevronDown rafc added

---
+++ src/components/Icons/IconChevronDown.jsx
@@ -0,0 +1,21 @@
+export const IconChevronDown = ({size = 16, color = "white"}) => {
+  return (
+    <>
+      <svg
+        width={size}
+        height={size}
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M4 6L8 10L12 6"
+          stroke={color}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </>
+  );
+};
