b70acc23671c3b1b874404fb61226ec6141ace62
dyatskovskiy <d.yatskovskiy@ukr.net>
2024-05-14 15:39:11 +0300

    fix calcRequiredWater

--- src/helpers/calcRequiredWater.js
+++ src/helpers/calcRequiredWater.js
@@ -1,6 +1,6 @@
 export const calcRequiredWater = (gender, weight, activeTime) => {
   if (gender === 'Man') {
-    const value = Number(weight) * 0.05 + Number(activeTime) * 0.6;
+    const value = Number(weight) * 0.04 + Number(activeTime) * 0.6;
     return Math.round(value * 10) / 10;
   }
   const value = Number(weight) * 0.03 + Number(activeTime) * 0.4;
