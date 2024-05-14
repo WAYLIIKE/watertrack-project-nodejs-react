32d38a05ba365624d38d72b5f5175c062cb2a34a
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-05-15 02:15:50 +0300

    div changed to react fragment

--- src/components/Icons/IconLogout.jsx
+++ src/components/Icons/IconLogout.jsx
@@ -2,8 +2,8 @@ import { FiLogOut } from 'react-icons/fi';
 
 export const IconLogout = () => {
   return (
-    <div>
+    <>
       <FiLogOut size={16} />
-    </div>
+    </>
   );
 };
--- src/components/Icons/IconSettings.jsx
+++ src/components/Icons/IconSettings.jsx
@@ -2,8 +2,8 @@ import { FiSettings } from 'react-icons/fi';
 
 export const IconSettings = () => {
   return (
-    <div>
+    <>
       <FiSettings size={16} />
-    </div>
+    </>
   );
 };
