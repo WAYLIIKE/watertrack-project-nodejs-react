1b883d6636cc1ea9101cea563a8c83b0a1488a39
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2024-05-15 02:56:18 +0200

    update redux. Update styles

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,11 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { refresh, signIn, signOut, signUp } from './userOps';
+import { signIn, signOut, signUp, current } from './userOps';
 import toast from 'react-hot-toast';
 
 const userSlice = createSlice({
   name: 'user',
   initialState: {
     user: {
+      _id: null,
       name: null,
       email: null,
       avatarURL: null,
@@ -13,6 +14,8 @@ const userSlice = createSlice({
       weight: null,
       activityTime: null,
       desiredVolume: null,
+      createdAt: null,
+      updatedAt: null,
     },
     accessToken: null,
     refreshToken: null,
@@ -22,19 +25,21 @@ const userSlice = createSlice({
   extraReducers: (builder) =>
     builder
       .addCase(signUp.fulfilled, (state, action) => {
-        console.log(action.payload);
         toast.success('Successfully registered!', {
           duration: 5000,
           position: 'top-center',
         });
       })
       .addCase(signIn.fulfilled, (state, action) => {
+        console.log(action.payload);
+        state.user = action.payload.user;
         state.accessToken = action.payload.accessToken;
         state.refreshToken = action.payload.refreshToken;
         state.isLoggedIn = true;
       })
       .addCase(signOut.fulfilled, (state) => {
         state.user = {
+          _id: null,
           name: null,
           email: null,
           avatarURL: null,
@@ -42,21 +47,22 @@ const userSlice = createSlice({
           weight: null,
           activityTime: null,
           desiredVolume: null,
+          createdAt: null,
+          updatedAt: null,
         };
         state.accessToken = null;
         state.refreshToken = null;
         state.isLoggedIn = false;
       })
-      .addCase(refresh.pending, (state) => {
+      .addCase(current.pending, (state) => {
         state.isRefreshing = true;
       })
-      .addCase(refresh.fulfilled, (state, action) => {
-        state.accessToken = action.payload.accessToken;
-        state.refreshToken = action.payload.refreshToken;
+      .addCase(current.fulfilled, (state, action) => {
+        state.user = action.payload.user;
         state.isLoggedIn = true;
         state.isRefreshing = false;
       })
-      .addCase(refresh.rejected, (state) => {
+      .addCase(current.rejected, (state) => {
         state.isRefreshing = false;
       }),
 });
