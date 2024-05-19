1f56523b5f2bb347efdab852805a9e95abddbc62
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-05-19 03:32:20 +0300

    DeleteWaterModal now deletes WaterItems

--- src/components/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/DeleteWaterModal/DeleteWaterModal.jsx
@@ -1,13 +1,29 @@
+import { useDispatch } from 'react-redux';
+import { deleteWater } from '../../redux/water/waterOps';
 import css from './DeleteWaterModal.module.css';
 
-export const DeleteWaterModal = ({ title }) => {
+export const DeleteWaterModal = ({ title, id, closeDeleteModal }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = async () => {
+    const response = await dispatch(deleteWater(id));
+
+    if (response.meta.requestStatus === 'fulfilled') closeDeleteModal();
+  };
+
+  const handleClose = () => {
+    closeDeleteModal();
+  };
+
   return (
     <div className={css.container}>
       <h2 className={css.title}>{title}</h2>
       <p className={css.question}>Are you sure you want to delete the entry?</p>
       <div className={css.buttonWrapper}>
-        <button className={css.deleteButton}>Delete</button>
-        <button className={css.cancelButton}>
+        <button className={css.deleteButton} onClick={handleDelete}>
+          Delete
+        </button>
+        <button className={css.cancelButton} onClick={handleClose}>
           <span className={css.cancelButtonText}>Cancel</span>
         </button>
       </div>
