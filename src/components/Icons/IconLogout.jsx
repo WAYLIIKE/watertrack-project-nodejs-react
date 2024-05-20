0c44a1ceebf01d21888fb1527f1fae946d9255a7
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-05-20 22:20:58 +0300

    IconLogout reworked

--- src/components/Icons/IconLogout.jsx
+++ src/components/Icons/IconLogout.jsx
@@ -1,9 +1,37 @@
-import { FiLogOut } from 'react-icons/fi';
-
-export const IconLogout = () => {
+export const IconLogout = ({ className, size = 16 }) => {
   return (
-    <>
-      <FiLogOut size={16} />
-    </>
+    <svg
+      width={size}
+      height={size}
+      className={`icon-logout ${className}`}
+      viewBox="0 0 16 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6 14.5H3.33333C2.97971 14.5 2.64057 14.3595 2.39052 14.1095C2.14048 13.8594 2 13.5203 2 13.1667V3.83333C2 3.47971 2.14048 3.14057 2.39052 2.89052C2.64057 2.64048 2.97971 2.5 3.33333 2.5H6"
+        className="icon-stroke"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.667 11.8334L14.0003 8.50002L10.667 5.16669"
+        className="icon-stroke"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14 8.5H6"
+        className="icon-stroke"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
   );
 };
